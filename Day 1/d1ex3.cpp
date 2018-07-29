9f12bffbbe6a8e58eb34a9153b01afa661dbda0f
JoMedeiros <josivan.medeiros@yahoo.com.br>
2018-07-29 11:00:02 -0300

    updates d1ex3

--- Day 1/d1ex3.cpp
+++ Day 1/d1ex3.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 class Map {
   public:
     int **map;
@@ -15,9 +17,34 @@ class Map {
       }
       delete this->map;
     }
+    /* Exercício bônus */
+    /* Essa função deve imprimir os elementos da matriz */
+    void print() {
+      /* Coloque seu código aqui */
+      for (int i = 0; i < 16; ++i) {
+        for (int j = 0; j < 16; ++j) {
+          std::cout << map[i][j] << " ";
+        }
+        std::cout << std::endl;
+      }
+    }
+    /* Essa função deve alterar uma célula da matriz */
+    void strike(int x, int y) {
+      /* Coloque seu código aqui */
+      ++map[x][y];
+    }
 };
 
 int main() {
   Map map;
+  int x, y;
+  while (true) /* loop infinito */{
+    map.print();
+    std::cout << "Digite coordenada x:\n";
+    std::cin >> x;
+    std::cout << "Digite coordenada y:\n";
+    std::cin >> y;
+    map.strike(x, y);
+  }
   return 0;
 }
